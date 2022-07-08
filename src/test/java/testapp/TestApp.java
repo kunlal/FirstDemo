d5a740e5bb6c7ba981e41ea50b6596db08a7501a
kundan <kundanlalcdac@gmail.com>
2022-07-08 23:52:47 +0530

    commited 1

--- src/test/java/testapp/TestApp.java
+++ src/test/java/testapp/TestApp.java
@@ -19,7 +19,7 @@ public class TestApp {
 		
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
+		driver = driver.manage().window().maximize();
 		driver.get("https://getbootstrap.com/docs/4.0/components/dropdowns/");
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		driver.findElement(By.id("dropdownMenuButton")).click();
